ff92a21d3228b595ddee445308ad4be0ef2c872b
Juwon <juwonemmanuel22@gmail.com>
2023-06-26 15:58:46 +0100

    workflow: rename name for test
    - change run component test to run test

--- src/__tests__/components/Button.test.tsx
+++ src/__tests__/components/Button.test.tsx
@@ -35,3 +35,37 @@ describe('Button', () => {
         expect(button).toBeDisabled();
     });
 });
+
+
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import { render, screen, waitFor } from '@testing-library/react';
+// import App from '../../App';
+// import 'intersection-observer';
+// import { HelmetProvider } from 'react-helmet-async';
+
+// const Providers = ({ children }: any) => {
+//     HelmetProvider.canUseDOM = false;
+
+//     beforeAll(() => {
+//         class IntersectionObserverMock {
+//             observe = jest.fn();
+//             unobserve = jest.fn();
+//             // You can also provide any other necessary methods or properties here
+//         }
+//         window.IntersectionObserver = IntersectionObserverMock as any;
+//     });
+//     return (
+//         <HelmetProvider>
+//             <Router>{children}</Router>
+//         </HelmetProvider>
+//     );
+// };
+
+// test('renders the app', async () => {
+//     render(<App />, { wrapper: Providers });
+
+//     await waitFor(() => {
+//         const heroText = screen.getByTestId('hero-text');
+//         expect(heroText).toBeInTheDocument();
+//     });
+// });
