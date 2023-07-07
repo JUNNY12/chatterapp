51329e7a5562fc8788cb79807cda23d107712505
Juwon Emmanuel <juwonemmanuel22@gmail.com>
2023-07-07 19:08:30 +0100

    bugfix: fix the bug in the analytics page
    - fix the bug in the analytics page

--- src/pages/analytics/Highlights.tsx
+++ src/pages/analytics/Highlights.tsx
@@ -19,6 +19,8 @@ export const Highlights = (): React.JSX.Element => {
    )
       return <AnalyticsLoader />;
 
+   console.log(filteredPosts);
+   console.log(highestViewedLikedAndCommentedPost);
    const createdDate = new Date(filteredPosts[0]?.data?.createdAt);
 
    const currentDate = new Date();
@@ -33,61 +35,72 @@ export const Highlights = (): React.JSX.Element => {
          }
         `}
       >
-         <div className=" border-b-2 pb-3 border-pink-600">
-            <Typography variant={1} className={`text-2xl font-bold mb-3`}>
-               Post Analytics
-            </Typography>
-
-            <Typography variant={2} className={`text-base font-normal`}>
-               <span> {formatDate(filteredPosts[0]?.data?.createdAt)}, </span>
-               <span> {getTimeDifferenceString(timeDifference)} </span>
-            </Typography>
-         </div>
-
-         <div className=" flex items-center">
-            <Typography variant={1} className={`text-xl font-bold mb-6 mt-6`}>
-               Top Post
-            </Typography>
-
-            <Typography variant={2} className={`ms-12 text-lg font-normal`}>
-               earned {filteredPosts[0]?.data?.views} views
-            </Typography>
-         </div>
-
-         <div>
-            <article className="tabletXS:my-3 mobileXL:px-2 cursor-pointer">
-               <div>
-                  <Typography variant={1} className=" text-3xl mobileXL:text-xl font-bold mb-2">
-                     {filteredPosts[0]?.data?.title}
+         {filteredPosts.length > 0 && (
+            <div>
+               <div className=" border-b-2 pb-3 border-pink-600">
+                  <Typography variant={1} className={`text-2xl font-bold mb-3`}>
+                     Post Analytics
                   </Typography>
 
-                  <Typography variant={2} className=" text-2xl mobileXL:text-lg font-normal mb-2">
-                     {filteredPosts[0]?.data?.subtitle.substring(0, 75) + ' ... '}
+                  <Typography variant={2} className={`text-base font-normal`}>
+                     <span> {formatDate(filteredPosts[0]?.data?.createdAt)}, </span>
+                     <span> {getTimeDifferenceString(timeDifference)} </span>
                   </Typography>
-
-                  <Typography variant={2} className="font-semibold mb-1">
-                     {calculateReadingTime(filteredPosts[0]?.data?.body)} mins read
-                  </Typography>
-
-                  <div className="flex flex-wrap items-center my-3">
-                     {filteredPosts[0]?.data?.tagList.map((tag: string, index: number) => (
-                        <div key={index} className="me-2">
-                           <span>#</span>
-                           <span className=" text-sm font-semibold text-pink-600">{tag}</span>
-                        </div>
-                     ))}
-                  </div>
-                  <div className=" relative object-cover max-w-[600px] h-[300px] my-3">
-                     <img
-                        src={filteredPosts[0]?.data?.coverImage}
-                        alt={filteredPosts[0]?.data?.title}
-                        className=" object-cover h-full w-full"
-                     />
-                  </div>
                </div>
-            </article>
-         </div>
 
+               <div className=" flex items-center">
+                  <Typography variant={1} className={`text-xl font-bold mb-6 mt-6`}>
+                     Top Post
+                  </Typography>
+
+                  <Typography variant={2} className={`ms-12 text-lg font-normal`}>
+                     earned {filteredPosts[0]?.data?.views} views
+                  </Typography>
+               </div>
+
+               <div>
+                  <article className="tabletXS:my-3 mobileXL:px-2 cursor-pointer">
+                     <div>
+                        <Typography
+                           variant={1}
+                           className=" text-3xl mobileXL:text-xl font-bold mb-2"
+                        >
+                           {filteredPosts[0]?.data?.title}
+                        </Typography>
+
+                        <Typography
+                           variant={2}
+                           className=" text-2xl mobileXL:text-lg font-normal mb-2"
+                        >
+                           {filteredPosts[0]?.data?.subtitle.substring(0, 75) + ' ... '}
+                        </Typography>
+
+                        <Typography variant={2} className="font-semibold mb-1">
+                           {calculateReadingTime(filteredPosts[0]?.data?.body)} mins read
+                        </Typography>
+
+                        <div className="flex flex-wrap items-center my-3">
+                           {filteredPosts[0]?.data?.tagList.map((tag: string, index: number) => (
+                              <div key={index} className="me-2">
+                                 <span>#</span>
+                                 <span className=" text-sm font-semibold text-pink-600">{tag}</span>
+                              </div>
+                           ))}
+                        </div>
+                        {filteredPosts[0]?.data?.coverImage && (
+                           <div className=" relative object-cover max-w-[600px] h-[300px] my-3">
+                              <img
+                                 src={filteredPosts[0]?.data?.coverImage}
+                                 alt={filteredPosts[0]?.data?.title}
+                                 className=" object-cover h-full w-full"
+                              />
+                           </div>
+                        )}
+                     </div>
+                  </article>
+               </div>
+            </div>
+         )}
          <div>
             <div className=" border-b-2 pb-3 border-pink-600">
                <Typography variant={1} className={`text-2xl mobileXL:text-xl font-bold  mt-6`}>
@@ -134,7 +147,7 @@ export const Highlights = (): React.JSX.Element => {
             {filteredPosts.length === 0 && (
                <div className="mt-4">
                   <Typography variant={3} className={`font-normal mb-3`}>
-                     <span> No Posts this </span>
+                     <span> No post yet this month</span>
                   </Typography>
                </div>
             )}
