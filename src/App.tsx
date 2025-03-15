65aee789b78db8025a295ed7b28edbb348464735
Juwon Emmanuel <juwonemmanuel22@gmail.com>
2025-03-15 04:01:07 +0100

    feat: Add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Suspense } from 'react';
 import { Loader } from './components/modules';
 import { ScrollToTop } from './components/modules';
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
    return (
       <Suspense fallback={<Loader />}>
          <Provider>
+            <Analytics />
             <Routes />
             <ToastContainer />
             <ScrollToTop />
--- src/components/layout/GeneralLayout.tsx
+++ src/components/layout/GeneralLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { Navbar, Footer } from '../modules';
 
+
 export const GeneralLayout = (): React.JSX.Element => {
    return (
       <>
